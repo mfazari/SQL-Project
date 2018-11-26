855dc8a9e7bb9cea7a0ba109884ea89de27dd3f2
mfazari <massimofazari@bluewin.ch>
2018-11-26 13:44:19 +0100

    first commit

---
+++ src/main.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+
+public class main {
+    public static void main(String args[]) throws SQLException {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/classicmodels?autoReconnect=true&useSSL=false", "root", "localhost");
+        Statement stmt = conn.createStatement();
+
+        {
+            ResultSet rs = stmt.executeQuery("SELECT * FROM classicmodels.employees;");
+            while (rs.next()) {
+                System.out.println(rs.getString("lastName"));
+            }
+        }
+    }
+}
