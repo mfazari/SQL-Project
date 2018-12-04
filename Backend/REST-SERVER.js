42aaadca12d446c127d618ac9e08ad5b7b4f1a45
mfazari <massimofazari@bluewin.ch>
2018-12-04 18:41:47 +0100

    JSON Object

--- Backend/REST-SERVER.js
+++ Backend/REST-SERVER.js
@@ -20,21 +20,21 @@ con.connect(function(err) {
 //URI to API
 var i;
 var days = ["Monday", "Tuesday", "Wednesday", "Thursdays", "Friday"];
+
+//Create LINKS
 for(i = 0; i < days.length; i++) {
-    var day = days[i];
     var menu_number;
-    for(menu_number=1; menu_number < 3; menu_number++) {
+    for(menu_number= 1; menu_number < 3; menu_number++) {
         var get = '/' + days[i] + '/' + menu_number;
         var statement = 'SELECT Description FROM '+ days[i] + ' WHERE Primary_Number = ' + menu_number + ';';
         app.get(get, function (req, res) {
             con.query(statement, function (error, results) {
                 if (error) throw error;
-                res.send(JSON.stringify(results));
+                res.send(results[0]);
             })
         });
     }
 }
-
 
 
 
